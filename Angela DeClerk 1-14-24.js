17c3241c94e7bc931eaf75394d1b9613ebd26990
Adeclerk9 <ang_declerk@yahoo.com>
2024-01-14 19:34:44 -0600

    testing

---
+++ Angela DeClerk 1-14-24.js
@@ -0,0 +1,17 @@
+console.log("testing now")
+
+var testing = 9;
+
+testing += 1;
+
+console.log(testing)
+
+console.log("favorite things")
+
+const games = games
+const toys = toys
+
+// What are your favorite things?
+
+games + toys; "favorite things";
+
