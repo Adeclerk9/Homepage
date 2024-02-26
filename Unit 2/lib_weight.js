92528204463876873518b994cf0d0fba553bd50d
Adeclerk9 <ang_declerk@yahoo.com>
2024-02-25 22:17:34 -0600

    Unit 2

---
+++ Unit 2/lib_weight.js
@@ -0,0 +1,8 @@
+
+const weight = {
+    pounds : 0,
+    convert : 0,
+    calculate() {
+        return this.pounds / this.convert;
+    }
+}
---
+++ Unit 2/objects.js
@@ -0,0 +1,4 @@
+
+weight.pounds = 56;
+weight.convert = 2.2046;
+console.log(weight.calculate());
